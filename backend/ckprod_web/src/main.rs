83c6a99e9a818513b20e62763a456fbbb2b08dba
sydhds <sydhds@gmail.com>
2026-05-07 15:04:33 +0200

    Cargo fmt

--- backend/ckprod_web/src/main.rs
+++ backend/ckprod_web/src/main.rs
@@ -1,4 +1,4 @@
-use axum::{response::Html, routing::get, Router};
+use axum::{Router, response::Html, routing::get};
 
 #[tokio::main]
 async fn main() {
@@ -19,30 +19,32 @@ async fn handler() -> Html<&'static str> {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::postgres::PgPoolOptions;
     use super::*;
+    use sqlx::postgres::PgPoolOptions;
 
     #[tokio::test]
     async fn test_db_select() -> anyhow::Result<()> {
-
         let db_url_default = "postgres://user:password@localhost:5432".to_string();
-        let db_url = format!("{}/{}", std::env::var("DATABASE_URL").unwrap_or(db_url_default), "postgres");
+        let db_url = format!(
+            "{}/{}",
+            std::env::var("DATABASE_URL").unwrap_or(db_url_default),
+            "postgres"
+        );
         println!("Using db_url: {}", db_url);
 
         let pool = PgPoolOptions::new()
             .max_connections(5)
-            .connect(db_url.as_str()).await?;
+            .connect(db_url.as_str())
+            .await?;
 
         // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
         let row: (i64,) = sqlx::query_as("SELECT $1")
             .bind(150_i64)
-            .fetch_one(&pool).await?;
+            .fetch_one(&pool)
+            .await?;
 
         assert_eq!(row.0, 150);
 
         Ok(())
-
     }
-
-
 }
