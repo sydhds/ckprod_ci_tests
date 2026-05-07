1dba6d58732c0c98afbd83923af468e8300ecacf
sydhds <sydhds@gmail.com>
2026-05-07 11:59:49 +0200

    Add unit test with db + CI db service

--- backend/ckprod_web/src/main.rs
+++ backend/ckprod_web/src/main.rs
@@ -16,3 +16,33 @@ async fn main() {
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::postgres::PgPoolOptions;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_db_select() -> anyhow::Result<()> {
+
+        let db_url_default = "postgres://user:password@localhost:5432".to_string();
+        let db_url = format!("{}/{}", std::env::var("DATABASE_URL").unwrap_or(db_url_default), "postgres");
+        println!("Using db_url: {}", db_url);
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(db_url.as_str()).await?;
+
+        // Make a simple query to return the given parameter (use a question mark `?` instead of `$1` for MySQL/MariaDB)
+        let row: (i64,) = sqlx::query_as("SELECT $1")
+            .bind(150_i64)
+            .fetch_one(&pool).await?;
+
+        assert_eq!(row.0, 150);
+
+        Ok(())
+
+    }
+
+
+}
